966c05b1455bf3c35b7b1f9668a8a7fd6b299706
Izanami <chinparra.alex@gmail.com>
2016-04-19 12:41:38 -0500

    -add the configuration of pretty url's -change the configuration of db

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ajax',
-    'username' => 'ajax',
-    'password' => 'WJPFVRQy36rtRrTE',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
