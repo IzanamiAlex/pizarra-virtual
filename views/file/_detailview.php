1e1714b572416de503d0657879321fcf907bf324
Warsaw01 <tonyalonzoc@gmail.com>
2016-05-07 16:52:17 -0500

    Show PDF file
    Shows PDF in File index.

--- views/file/_detailview.php
+++ views/file/_detailview.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use wahyugnc\pdfjs\ViewerPDF; 
 
 /* @var $this yii\web\View */
 /* @var $model app\models\File */
@@ -23,37 +24,41 @@ $this->title = $model->name;
             'value' => Html::a($model->file_name,'@web/files/files/'.$model->file_name),
             'format' => 'html',
         ],
-        ],
-    ]) ?>
+    ],
+    ]) 
+    ?>
 
-<?php 
+    <?php 
     $extension = pathInfo($model->file_name, PATHINFO_EXTENSION); 
+
+        switch($extension) {
+            case 'bmp':
+            case 'jpeg':
+            case 'jpg':
+            case 'png':
+                echo '<h2>File Preview</h2>'; 
+                echo Html::img(Yii::getAlias('@web').'/files/files/'. $model->file_name);
+                break;
+            case 'pdf':
+                echo '<iframe src="'.Yii::getAlias('@web').'/files/files/'.$model->file_name.'" height="1000" width="900"></iframe>';
+                break;
+            default:
+                break;
+        }
+    ?>
+
+    <?php if(!empty($model->group)): ?>
     
-    switch($extension) {
-        case 'bmp':
-        case 'jpeg':
-        case 'jpg':
-        case 'png':
-            echo '<h2>File Preview</h2>'; 
-            echo Html::img(Yii::getAlias('@web').'/files/files/'. $model->file_name);
-            break;
-        default:
-            break;
-    }
-?>
+    <h2>Group</h2>
 
-<?php if(!empty($model->group)): ?>
-
-<h2>Group</h2>
-
-<?= DetailView::widget([
+    <?= DetailView::widget([
     'model' => $model->group,
     'attributes' => [
         'id',
         'name',  
     ],
-]) ?>
+    ]) ?>
 
-<?php endif; ?>
+    <?php endif; ?>
 
 </div>
