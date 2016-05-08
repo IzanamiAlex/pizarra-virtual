cd6a613791640452938709f91d86ee920c000498
Warsaw01 <tonyalonzoc@gmail.com>
2016-05-07 21:30:07 -0500

    Upload mp3 files
    Updated model to allow mp3 extension and also updated _detailview to
    show audio player in case of mp3 file.
    
    *To upload files that exceed 2M, search for line
    "upload_max_filesize=2M" in php.ini and change its value to required
    value and restart Apache.

--- views/file/_detailview.php
+++ views/file/_detailview.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-use wahyugnc\pdfjs\ViewerPDF; 
 
 /* @var $this yii\web\View */
 /* @var $model app\models\File */
@@ -29,21 +28,27 @@ $this->title = $model->name;
     ?>
 
     <?php 
-    $extension = pathInfo($model->file_name, PATHINFO_EXTENSION); 
+    $file_location = Yii::getAlias('@web') . '/files/files/';
+    $file_name = $model -> file_name;
+    $file_src =  $file_location . $file_name;
 
-        switch($extension) {
-            case 'bmp':
-            case 'jpeg':
-            case 'jpg':
-            case 'png':
-                echo '<h2>File Preview</h2>'; 
-                echo Html::img(Yii::getAlias('@web').'/files/files/'. $model->file_name);
-                break;
-            case 'pdf':
-                echo '<iframe src="'.Yii::getAlias('@web').'/files/files/'.$model->file_name.'" height="1000" width="900"></iframe>';
-                break;
-            default:
-                break;
+    $extension = pathInfo($model->file_name, PATHINFO_EXTENSION); 
+    switch($extension) {
+        case 'bmp':
+        case 'jpeg':
+        case 'jpg':
+        case 'png':
+            echo '<h2>File Preview</h2>'; 
+            echo Html::img($file_src);
+            break;
+        case 'pdf':
+            echo '<iframe src="' . $file_src . '" height="1000" width="900"></iframe>';
+            break; 
+        case 'mp3':
+            echo '<audio controls><source src="' . $file_src . '"</audio>';
+            break;
+        default:
+            break;
         }
     ?>
 
